4e766156ade0c3b0b02c8f34a14d4145a4f653a4
vcsil <vcsilva@poil.ufrj.br>
2024-03-01 21:30:24 -0300

    :sparkles: feat: create dashboard page

---
+++ src/pages/Dashboard/DashboarPage.tsx
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+import { DateRangePicker } from "@/pages/Dashboard/Dashboard/date-range-picker/date-range-picker";
+import Dashboard from "@/pages/Dashboard/Dashboard/Dashboard";
+
+export default function DashboardPage() {
+    return (
+        <>
+            <Titulo>Dashboard</Titulo>
+            <Subtitulo>
+                Ultima atualização há: <span>30 minutos</span>
+            </Subtitulo>
+            <Dashboard />
+            <h1>iuu</h1>
+        </>
+    );
+}
+
+const Titulo = styled.h1`
+    width: 208px;
+    font-weight: 700;
+    font-size: 40px;
+`;
+
+const Subtitulo = styled.p`
+    span {
+        color: #d88225;
+    }
+`;
