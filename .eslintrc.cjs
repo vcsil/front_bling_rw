34b4276f612ac10fd48a6701683471060f9e7912
vcsil <vcsilva@poil.ufrj.br>
2024-03-01 21:04:25 -0300

    :wrench: config: add config files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,12 +7,13 @@ module.exports = {
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
     'plugin:import/recommended',
+    'plugin:import/typescript',
     'plugin:jsx-a11y/recommended',
     'plugin:@typescript-eslint/recommended-type-checked',
     'plugin:@typescript-eslint/stylistic',
     'eslint-config-prettier',
   ],
-  ignorePatterns: ['node_modelus','vite-env.d.ts','dist', '.eslintrc.cjs'],
+  ignorePatterns: ['node_modelus','vite-env.d.ts','dist', '.eslintrc.cjs', 'vite.config.ts'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
@@ -20,7 +21,7 @@ module.exports = {
     project: ['./tsconfig.json', './tsconfig.node.json'],
     tsconfigRootDir: __dirname,
   },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'import'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
@@ -35,6 +36,23 @@ module.exports = {
         'argsIgnorePattern': '^_'
       }
     ],
-    'react/react-in-jsx-scope': 'off'
+    'react/react-in-jsx-scope': 'off',
+    'import/no-unresolved': 'error',
   },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx']
+    },
+    'import/resolver': {
+      typescript: {
+        'alwaysTryTypes': true, // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
+
+        // Choose from one of the 'project' configs below or omit to use <root>/tsconfig.json by default
+
+        // use <root>/path/to/folder/tsconfig.json
+        'project': './tsconfig.json',
+      },
+      node: true
+    }
+  }
 }
