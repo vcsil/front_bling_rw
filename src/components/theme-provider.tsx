0c12dd41e87fe1c24ec3de49b20a27743cc6b73e
vcsil <vcsilva@poil.ufrj.br>
2024-03-01 21:28:49 -0300

    :sparkles: feat: add toggle theme button

---
+++ src/components/theme-provider.tsx
@@ -0,0 +1,82 @@
+import { createContext, useEffect, useState } from "react";
+
+type Theme = "dark" | "light" | "system";
+
+interface ThemeProviderProps {
+    children: React.ReactNode;
+    defaultTheme?: Theme;
+    storageKey?: string;
+}
+
+interface ThemeColors {
+    primary: string;
+    secondary: string;
+    border: string;
+}
+
+interface ThemeProviderState {
+    theme: Theme;
+    setTheme: (_theme: Theme) => void;
+    colors: ThemeColors;
+}
+
+const initialState: ThemeProviderState = {
+    theme: "system",
+    setTheme: () => null,
+    colors: ColorsDefaultByTheme("light"),
+};
+
+export const ThemeProviderContext = createContext<ThemeProviderState>(initialState);
+
+function ColorsDefaultByTheme(themeName: string): ThemeColors {
+    if (themeName === "light") {
+        return {
+            primary: "#18181b",
+            secondary: "#f4f4f5",
+            border: "#27272a", //"#e4e4e7",
+        };
+    } else if (themeName === "dark") {
+        return {
+            primary: "#f4f4f5",
+            secondary: "#18181b",
+            border: "#e4e4e7",
+        };
+    } else {
+        const systemTheme = window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
+        return ColorsDefaultByTheme(systemTheme);
+    }
+}
+
+export function ThemeProvider({ children, defaultTheme = "system", storageKey = "vite-ui-theme", ...props }: ThemeProviderProps) {
+    const [theme, setTheme] = useState<Theme>(() => (localStorage.getItem(storageKey) as Theme) || defaultTheme);
+
+    useEffect(() => {
+        const root = window.document.documentElement;
+
+        root.classList.remove("light", "dark");
+
+        if (theme === "system") {
+            const systemTheme = window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
+
+            root.classList.add(systemTheme);
+            return;
+        }
+
+        root.classList.add(theme);
+    }, [theme]);
+
+    const value = {
+        theme,
+        setTheme: (theme: Theme) => {
+            localStorage.setItem(storageKey, theme);
+            setTheme(theme);
+        },
+        colors: ColorsDefaultByTheme(theme),
+    };
+
+    return (
+        <ThemeProviderContext.Provider {...props} value={value}>
+            {children}
+        </ThemeProviderContext.Provider>
+    );
+}
