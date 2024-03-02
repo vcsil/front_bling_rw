4baf325c1f4c78d7dfff6402047cfe86575fffa2
vcsil <vcsilva@poil.ufrj.br>
2024-03-01 21:09:49 -0300

    :sparkles: feat: add ui lib shadcn

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./app/App.tsx";
-import "./index.css";
+
+import { ThemeProvider } from "@/components/theme-provider";
+import "@/shared/styles/global.css";
+import App from "@/app/App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
+    <ThemeProvider>
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    </ThemeProvider>,
 );
