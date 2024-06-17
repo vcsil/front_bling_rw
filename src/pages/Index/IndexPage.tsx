3e879c2aca6aa4c5d0b5d32af7e559b39f6e3b22
vcsil <vcsilva@poli.ufrj.br>
2024-06-17 19:17:51 -0300

    :wrench: chore: redirect vite image

--- src/pages/Index/IndexPage.tsx
+++ src/pages/Index/IndexPage.tsx
@@ -4,7 +4,6 @@ import styled from "styled-components";
 import { Button } from "@/components/ui/button";
 
 import reactLogo from "@/assets/react.svg";
-import viteLogo from "/vite.svg";
 
 function IndexPage() {
     const [count, setCount] = useState(0);
@@ -13,7 +12,7 @@ function IndexPage() {
         <Raiz>
             <div>
                 <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
+                    <img src="./vite.svg" className="logo" alt="Vite logo" />
                 </a>
                 <a href="https://react.dev" target="_blank" rel="noreferrer">
                     <img src={reactLogo} className="logo react" alt="React logo" />
