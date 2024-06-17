bd906bbe0b3b50f873df1212ee8496dc9dabcab2
vcsil <vcsilva@poli.ufrj.br>
2024-06-17 19:14:18 -0300

    :fire: remove: delete tests pages

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -2,7 +2,6 @@ import { Routes, Route } from "react-router-dom";
 
 import DashboardPage from "@/pages/Dashboard/DashboarPage";
 import IndexPage from "@/pages/Index/IndexPage";
-import MailPage from "@/pages/Teste/page";
 import { Layout } from "@/Layout";
 
 function AppRoutes() {
@@ -10,15 +9,6 @@ function AppRoutes() {
         <Layout>
             <Routes>
                 <Route path="/" element={<IndexPage />} />
-                <Route
-                    path="/teste"
-                    element={
-                        <div className="overflow-hidden rounded-lg border bg-background shadow-lg">
-                            <MailPage />
-                        </div>
-                    }
-                />
-                <Route path="/teste2" element={<h1>Oi</h1>} />
                 <Route path="/dashboard" element={<DashboardPage />} />
 
                 <Route path="*" element={<h1>404</h1>} />
