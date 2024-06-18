0ba88bdb44b458db5d3e9e3e4f6a5f3d380ba8e8
vcsil <vcsilva@poli.ufrj.br>
2024-06-18 16:29:45 -0300

    :wrench: chore: remove unused items

--- src/Layout/constants/sideNav.tsx
+++ src/Layout/constants/sideNav.tsx
@@ -23,31 +23,4 @@ export const NavItems: NavItem[] = [
             },
         ],
     },
-    {
-        title: "Example",
-        icon: BookOpenCheck,
-        href: "/example",
-        color: "text-orange-500",
-        isChidren: true,
-        children: [
-            {
-                title: "Example-01",
-                icon: BookOpenCheck,
-                color: "text-red-500",
-                href: "/example/employees",
-            },
-            {
-                title: "Example-02",
-                icon: BookOpenCheck,
-                color: "text-red-500",
-                href: "/example/example-02",
-            },
-            {
-                title: "Example-03",
-                icon: BookOpenCheck,
-                color: "text-red-500",
-                href: "/example/example-03",
-            },
-        ],
-    },
 ];
