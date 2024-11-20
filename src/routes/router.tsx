ad303a05153623ae1358a6a219291363368ed982
vcsil <vcsilva@poli.ufrj.br>
2024-11-20 16:11:12 -0300

    :fire: remove index page react

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,7 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 
 import DashboardPage from "@/pages/Dashboard/DashboarPage";
-import IndexPage from "@/pages/Index/IndexPage";
 import { Layout } from "@/Layout";
 import BalancoPage from "@/pages/Balanco/BalancoPage";
 import CompareBalancePage from "@/pages/Balanco/CompareBalance/CompareBalance";
@@ -11,7 +10,6 @@ function AppRoutes() {
     return (
         <Layout>
             <Routes>
-                <Route path="/" element={<IndexPage />} />
                 <Route path="/dashboard" element={<DashboardPage />} />
                 <Route path="/balanco" element={<BalancoPage />} />
                 <Route path="/balanco/compare" element={<CompareBalancePage />} />
