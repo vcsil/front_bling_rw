c42154751a8867a6ef70121fa9409bd7c64a800f
vcsil <vcsilva@poli.ufrj.br>
2024-12-03 18:00:57 -0300

    :wrench: chore: adds space at the bottom of the application

--- src/Layout/index.tsx
+++ src/Layout/index.tsx
@@ -7,7 +7,7 @@ export const Layout = ({ children }: { children: React.ReactNode }) => {
             <Header />
             <div className="flex h-screen border-collapse overflow-hidden">
                 <Sidebar className="" />
-                <main className="flex-1 overflow-y-auto overflow-x-hidden pt-16 bg-secondary/10 pb-1 px-4">{children}</main>
+                <main className="flex-1 overflow-y-auto overflow-x-hidden pt-16 bg-secondary/10 pb-4 px-4">{children}</main>
             </div>
         </>
     );
