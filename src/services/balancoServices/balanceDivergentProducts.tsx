4479f3865c365c12986fd12eae6811d015b83628
vcsil <vcsilva@poli.ufrj.br>
2024-12-19 09:25:22 -0300

    :wrench: chore: rename interface

--- src/services/balancoServices/balanceDivergentProducts.tsx
+++ src/services/balancoServices/balanceDivergentProducts.tsx
@@ -1,9 +1,9 @@
 import api from "@/services/api";
 
-import { DivergentroductsT } from "@/pages/Balanco/types";
+import { DivergentProductsT } from "@/pages/Balanco/types";
 
-export async function getDivergentProducts(idDeposit: number, dateBalance: Date): Promise<DivergentroductsT[]> {
+export async function getDivergentProducts(idDeposit: number, dateBalance: Date): Promise<DivergentProductsT[]> {
     const response = await api.get(`/products/balance/divergences/${idDeposit}/${String(dateBalance)}`);
 
-    return response.data as DivergentroductsT[];
+    return response.data as DivergentProductsT[];
 }
