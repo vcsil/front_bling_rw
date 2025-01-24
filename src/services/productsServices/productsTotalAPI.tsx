251c64022970407d839fbd469d69c9ec9e4ec934
vcsil <vcsilva@poli.ufrj.br>
2025-01-24 15:33:05 -0300

    :bug: fix: show paginator when searching

--- src/services/productsServices/productsTotalAPI.tsx
+++ src/services/productsServices/productsTotalAPI.tsx
@@ -1,10 +1,26 @@
 import api from "@/services/api";
 
-export async function getProductsTotalQuantity(idCategory: number | undefined): Promise<{ total: number }> {
-    if (idCategory) {
+export async function getProductsTotalQuantity(idCategory: number | undefined, text = ""): Promise<{ total: number }> {
+    if (idCategory && text) {
+        const queryParams = {
+            idCategory,
+            text,
+        };
+
+        const response = await api.get(`/products/total`, { params: queryParams });
+        return response.data as { total: number };
+    } else if (idCategory) {
         const queryParams = {
             idCategory,
         };
+
+        const response = await api.get(`/products/total`, { params: queryParams });
+        return response.data as { total: number };
+    } else if (text) {
+        const queryParams = {
+            text,
+        };
+
         const response = await api.get(`/products/total`, { params: queryParams });
         return response.data as { total: number };
     } else {
